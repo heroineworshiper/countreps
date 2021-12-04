bd1b62a12eaa25ba64017e9a9ae51f199f9b7e3b
heroineworshiper <broadcast@earthling.net>
2021-12-04 12:39:53 -0800

    servos over USB

--- servos.X/build/default/production/servos.p1.d
+++
@@ -1,3 +0,0 @@
- build/default/production/servos.d  \
- build/default/production/servos.p1:  \
- servos.c 
--- servos.X/build/default/production/transceiver.p1.d
+++
@@ -1,3 +0,0 @@
- build/default/production/transceiver.d  \
- build/default/production/transceiver.p1:  \
- transceiver.c 
